3c37f3306db4197d0cebefc001a20465d005ee5f
Chung Nguyễn <nguyenthachbaochung@gmail.com>
2022-04-29 21:51:12 +0700

    Add project files.

---
+++ GWbook/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GWbook.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
